00d42d29df71b60a76f694f831006fc9b5d11cac
Александра Царькова <pravitel.mira@list.ru>
2021-04-28 17:38:56 +0300

    Initial Commit

---
+++ APICombineService/APICombineServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  APICombineServiceApp.swift
+//  APICombineService
+//
+//  Created by Александра on 28.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct APICombineServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
